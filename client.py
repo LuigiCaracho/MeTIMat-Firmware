3e0e9619c52083d172114ab48023f2859a48e992
LuigiCaracho <github.57939@8alias.com>
2026-01-22 17:11:02 +0100

    rgb upon Scan

--- client.py
+++ client.py
@@ -1,12 +1,18 @@
 import logging
+import os
+import sys
 import threading
+import time
 
 import requests
+
+from led.constants import GREEN, RED, WHITE
+from led.controller import LEDController
 
 logging.basicConfig(level=logging.INFO)
 
 
-def send_scan(url, qr_data):
+def send_scan(url: str, qr_data: str, led_controller: LEDController):
     """
     Sendet QR-Code an API, prüft Response nach QRValidationResponse-Format
     """
@@ -21,10 +27,16 @@ def send_scan(url, qr_data):
                     logging.info(
                         f"✅ QR gültig: {qr_data} – Profile: {data.get('profile')}"
                     )
+                    led_controller.update_color(GREEN)
+
+                    led_controller.set_timeout(9, WHITE)
                 else:
                     logging.warning(
                         f"❌ QR ungültig: {qr_data} – Message: {data.get('message')}"
                     )
+                    led_controller.update_color(RED)
+                    led_controller.set_timeout(9, WHITE)
+
             else:
                 logging.error(f"❌ POST {response.status_code}: {qr_data}")
         except Exception as e:
--- led/controller.py
+++ led/controller.py
@@ -10,6 +10,9 @@ from .constants import LED_COUNT, LOGO_BLUE, LOGO_TURQUOISE, STRIP_PARAMETERS
 class LEDController(Thread):
     strip: PixelStrip
     color: RGBW
+    target_color: RGBW
+    timeout: float
+    waiting: float = False
 
     def __init__(self, params: List[Any] = STRIP_PARAMETERS) -> None:
         super().__init__()
@@ -36,8 +39,17 @@ class LEDController(Thread):
         last_color = Color(0, 0, 0)
 
         while True:
+            if time.time() - self.timeout > 0 and self.waiting:
+                self.update_color(self.target_color)
+                self.waiting = False
+
             while self.color == last_color:
                 time.sleep(1)
 
             self.__set_all(self.color)
             last_color = self.color
+
+    def set_timeout(self, timeout: float, color: RGBW):
+        self.timeout = time.time() + timeout
+        self.waiting = True
+        self.target_color = color
