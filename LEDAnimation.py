f4b969fc5ecc680340413176715133a434f9f8c2
LuigiCaracho <github.57939@8alias.com>
2026-01-22 15:52:46 +0100

    fix: typo

--- LEDAnimation.py
+++ LEDAnimation.py
@@ -3,7 +3,7 @@ import time
 from rpi_ws281x import Color, PixelStrip, ws
 
 # PixelStrip initialisieren (wie vorher)
-STRIP = PixelStrip(15, 18, 800000, 10, False, 255, 0, ws.WS281_STRIP_GBR)
+STRIP = PixelStrip(15, 18, 800000, 10, False, 255, 0, ws.WS2811_STRIP_GBR)
 STRIP.begin()
 
 colors = [LOGO_BLUE, LOGO_TURQUOISE, WHITE]
